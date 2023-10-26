433b5828d0df698297440649415a6feae7de17c0
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-10-25 23:56:10 -0300

    feat: icons and colors import

--- Kotlin-app/app/build.gradle.kts
+++ Kotlin-app/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.kotlin_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kotlin_app"
--- Kotlin-app/app/src/main/java/com/example/kotlin_app/ui/theme/Color.kt
+++ Kotlin-app/app/src/main/java/com/example/kotlin_app/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.kotlin_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Orange1 = Color(0xFF9D2D15)
+val Orange2 = Color(0xFFB43717)
+val Orange3 = Color(0xFFD8491D)
+val Orange4 = Color(0xFFFE5433)
+val Orange5 = Color(0xFFFAB49E)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF292929)
+val Gray = Color(0xFF828282)
