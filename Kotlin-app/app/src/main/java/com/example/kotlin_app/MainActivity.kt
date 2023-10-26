6dec579899ec9244709d435622ecd1e3459e800c
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-10-25 22:29:00 -0300

    init: kotlin project init

---
+++ Kotlin-app/app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kotlin_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_app.ui.theme.KotlinappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinappTheme {
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinappTheme {
+        Greeting("Android")
+    }
+}
