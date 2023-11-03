10e4bdee3a3c39a5cec3384daf973c38cc59eb62
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-11-03 01:50:26 -0300

    feat: Login inputs

--- Kotlin-app/app/src/main/java/com/example/kotlin_app/presentation/login/LoginPage.kt
+++ Kotlin-app/app/src/main/java/com/example/kotlin_app/presentation/login/LoginPage.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_app.presentation.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,27 +12,48 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.example.kotlin_app.R
+import com.example.kotlin_app.ui.theme.WhiteGray
+import com.example.kotlin_app.ui.theme.Black
+
 
 @Composable
 fun LoginPage() {
+    var email by remember { mutableStateOf("") }
+    var emailPlaceholder by remember { mutableStateOf("Email") }
+    var password by remember { mutableStateOf("") }
+    var passwordPlaceholder by remember { mutableStateOf("Senha") }
+
     MaterialTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -38,7 +61,8 @@ fun LoginPage() {
         ) {
             Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceBetween
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Row(){
                     Image(
@@ -46,6 +70,83 @@ fun LoginPage() {
                         painter = painterResource(id = R.drawable.login_top_illustration),
                         contentDescription = null
                     )
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(.9f)
+                        .padding(horizontal = 16.dp),
+                ){
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                        ) {
+                                 BasicTextField(
+                                     value = email,
+                                     onValueChange = { email = it; emailPlaceholder = ""
+                                     },
+                                     modifier = Modifier
+                                         .fillMaxWidth()
+                                         .background(WhiteGray)
+                                         .padding(
+                                             top = 10.dp,
+                                             bottom = 10.dp,
+                                             start = 5.dp,
+                                             end = 5.dp
+                                         ),
+                                 ) {
+                                     Text(
+                                         text = email,
+                                         style = MaterialTheme.typography.bodyLarge,
+                                         color = Black,
+                                     )
+                                     Text(
+                                         text = emailPlaceholder,
+                                         style = MaterialTheme.typography.bodyLarge,
+                                         color = Color.Gray
+                                     )
+                                 }
+                             }
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            BasicTextField(
+                                value = password,
+                                onValueChange = { password = it; passwordPlaceholder = ""
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(WhiteGray)
+                                    .padding(
+                                        top = 10.dp,
+                                        bottom = 10.dp,
+                                        start = 5.dp,
+                                        end = 5.dp
+                                    ),
+                            ) {
+                                Text(
+                                    text = password,
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = Black,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                )
+                                Text(
+                                    text = passwordPlaceholder,
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = Color.Gray
+                                )
+                            }
+                        }
+                    }
                 }
                 Box() {
                     Row(
